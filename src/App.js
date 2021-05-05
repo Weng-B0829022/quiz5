a2d940c5713bc0153b4f89f96d6bceb81318c13c
Dennis <dennis1989999@gmail.com>
2021-05-05 20:12:08 +0800

    quiz5

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import multiButton from './cgu_multiButton.js'
+
+function App() {
+  return (
+      <div className="App">{multiButton(10)}</div>  
+);}
+
+export default App;
+
+
---
+++ src/cgu_multiButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+class CounterButton extends React.Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        counter: 0
+      }
+      this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(event) {
+      this.setState({ counter: this.state.counter+1 });
+      console.log(this.state);
+    }
+    render() {
+        var text = this.state.counter;
+        return (
+        <button onClick={this.handleClick}>你點了{text}次.</button>
+        );
+      }
+    }
+    
+const multiButton=(num)=>{
+    var output=[];
+    for(let i=1;i<num+1;++i)
+        output.push(<CounterButton/>)
+    return output;
+}
+    
+export default multiButton;
